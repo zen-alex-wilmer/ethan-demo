78fd7fa0e37dbcb8ea9626da733498f851cacaf3
Alex Wilmer <alex.wilmer@zensurance.com>
2023-10-12 21:14:27 -0600

    ethans' whatever site

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./App.css";
+
+window.onmousemove = (e) => {
+  document.body.style.backgroundColor = `hsl(${e.clientY}, 50%, 50%)`;
+};
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Ethan wants to learn to code</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          ethan's iq: {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
